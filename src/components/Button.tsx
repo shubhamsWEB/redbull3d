de3e4222b7a2078cbb0fd131254feb0be857e7a9
Devpod17 <shubham.agrawal.peopleinteractive.in>
2024-09-09 19:54:10 +0530

    fixes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,13 +9,14 @@ type Props = {
 export default function Button({ buttonLink, buttonText, className }: Props) {
   return (
     <button
-      className={clsx(
-        "rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold uppercase tracking-wide text-white transition-colors duration-150 hover:bg-orange-700 md:text-2xl",
-        className,
-      )}
-      
-    >
-      {buttonText}
-    </button>
+  onClick={() => window.open('https://shubhamsweb.netlify.app', '_blank')}
+  className={clsx(
+    "rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold uppercase tracking-wide text-white transition-colors duration-150 hover:bg-orange-700 md:text-2xl",
+    className,
+  )}
+>
+  {buttonText}
+</button>
+
   );
 }
